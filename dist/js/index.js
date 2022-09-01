cdc91c946948e0da36db4b9c735d4c16350dddc7
alexsonalmeida <101877352+alexsonalmeida@users.noreply.github.com>
2022-09-01 16:28:26 -0300

    adicao das primeiras verificacoes TypeScript

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,2 +1,39 @@
 "use strict";
-console.log("funcionou");
+const ourMarker = document.querySelector(".pontos1");
+const theirMarker = document.querySelector(".pontos2");
+let ourScore = 0;
+let theirScore = 0;
+function takePoints(isUs) {
+    if (isUs) {
+        if (ourScore === 0)
+            console.log("já está um placar vazio");
+        else {
+            ourScore--;
+            ourMarker.innerHTML = ourScore.toString();
+        }
+    }
+    else {
+        if (theirScore === 0)
+            console.log("já está um placar vazio");
+        else {
+            theirScore--;
+            theirMarker.innerHTML = theirScore.toString();
+        }
+    }
+}
+function addPoints(points, isUs) {
+    if (isUs) {
+        ourScore += points;
+        if (ourScore >= 12)
+            console.log("vencemos");
+        else
+            ourMarker.innerHTML = ourScore.toString();
+    }
+    else {
+        theirScore += points;
+        if (theirScore >= 12)
+            console.log("eles venceram");
+        else
+            theirMarker.innerHTML = theirScore.toString();
+    }
+}
